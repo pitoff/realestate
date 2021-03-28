39c211f7b289d54a7c99540875292cffb7a17085
pitoff <61664958+pitoff@users.noreply.github.com>
2021-03-28 02:44:55 +0100

    Update header.php

--- app/view/include/header.php
+++ app/view/include/header.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8">
+    <meta name="description" content="Vostan group">
+    <meta name="keywords" content="Vostan">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800,900&display=swap"
         rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900&display=swap" rel="stylesheet">
