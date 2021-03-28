b78b5ffe5517f2a372ab609bd96a67693f8462bb
pitoff <61664958+pitoff@users.noreply.github.com>
2021-03-28 14:13:56 +0100

    Update about.php

--- app/view/pages/about.php
+++ app/view/pages/about.php
@@ -80,8 +80,8 @@
     </section>
     <!-- About Section End -->
     
- <!-- Contact Section Begin -->
-    <section class="contact-section">
+    <!-- Contact Section Begin -->
+    <section class="contact-section" style="margin-top: 5px;">
         <div class="container">
             <div class="row">
                 <div class="col-lg-6">
@@ -92,7 +92,7 @@
                             </div>
                             <div class="ci-text">
                                 <h5>Address</h5>
-                                <p>160 Pennsylvania Ave NW, Washington, Castle, PA 16101-5161</p>
+                                <p>No 22 Okpara marginal avenue, mulumba state</p>
                             </div>
                         </div>
                         <div class="ci-item">
@@ -102,8 +102,7 @@
                             <div class="ci-text">
                                 <h5>Phone</h5>
                                 <ul>
-                                    <li>125-711-811</li>
-                                    <li>125-668-886</li>
+                                    <li>+234805544332211</li>
                                 </ul>
                             </div>
                         </div>
@@ -113,7 +112,7 @@
                             </div>
                             <div class="ci-text">
                                 <h5>Support</h5>
-                                <p>Support.aler@gmail.com</p>
+                                <p>vostan.support@vostan.com</p>
                             </div>
                         </div>
                     </div>
@@ -122,7 +121,7 @@
         </div>
         <div class="cs-map">
             <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d735515.5813275519!2d-80.41163541934742!3d43.93644386501528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882a55bbf3de23d7%3A0x3ada5af229b47375!2sMono%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1583262687289!5m2!1sen!2sbd"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d735515.5813275519!2d-80.41163541934742!3d43.93644386501528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882a55bbf3de23d7%3A0x3ada5af229b47375!2sAwka%2C%20Anambra%2C%20Nigeria!5e0!3m2!1sen!2sbd!4v1583262687289!5m2!1sen!2sbd"
                 height="450" style="border:0;" allowfullscreen=""></iframe>
         </div>
     </section>
