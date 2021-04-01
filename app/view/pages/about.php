cb93c50d8336d7025b2704eef571871d3dfd0b33
pitoff <61664958+pitoff@users.noreply.github.com>
2021-04-01 02:44:22 +0100

    Update about.php

--- app/view/pages/about.php
+++ app/view/pages/about.php
@@ -25,8 +25,7 @@
                     <div class="about-text">
                         <div class="at-title">
                             <h3>About company</h3>
-                            <p style="text-align: justify;"> <?php echo SITENAME;?> is a registered Real Estate Company in Nigeria dedicated to providing expedient world
-                            class services and market leading expertise for properties purchase, sales, building and rentals. At <?php echo SITENAME;?>, you will fid whatever you are looking for in your dream home and more, all at arate you can afford. We have relaible and highly qualified Lawyers, Surveyors, and Estate valuers to provide our clients with the best property experience.</p>
+                            <p style="text-align: justify;"> <?php echo SITENAME;?> is a registered Real Estate Company in Nigeria dedicated to providing expedient world class services and market leading expertise for property purchase, sales, building and rentals. At <?php echo SITENAME;?>, you will find whatever you are looking for in your dream home and more, all at a rate you can afford. We have relaible and highly qualified Lawyers, Surveyors, and Estate valuers to provide our clients with the best property experience.</p>
 
                     
 
@@ -35,7 +34,7 @@
                                 <div class="af-icon">
                                     <img src="../realestate/img/chooseus/chooseus-icon-1.png" alt="">
                                 </div>
-                                <div class="af-text" style="text-align: justify;">
+                                <div class="af-text">
                                     <h6>Our mission</h6>
                                     <p>To modernize and progress the experience of buying and selling of real estate by cultivating a spirit of collaboration, innovation, and Integrity</p>
                                 </div>
@@ -44,7 +43,7 @@
                                 <div class="af-icon">
                                     <img src="../realestate/img/chooseus/chooseus-icon-2.png" alt="">
                                 </div>
-                                <div class="af-text" style="text-align: justify;">
+                                <div class="af-text">
                                     <h6>Our Vision</h6>
                                     <p>To be the only TRUE real estate franchise that provides an EXCELLENT and CONSISTENT customer experience at all time.</p>
                                 </div>
@@ -53,7 +52,7 @@
                                 <div class="af-icon">
                                     <img src="../realestate/img/chooseus/chooseus-icon-3.png" alt="">
                                 </div>
-                                <div class="af-text" style="text-align: justify;">
+                                <div class="af-text">
                                     <h6>Our believe</h6>
 
                                     <p>1. We love what we do and it shows.</p>
@@ -68,7 +67,7 @@
                                 <div class="af-icon">
                                     <img src="../realestate/img/chooseus/chooseus-icon-4.png" alt="">
                                 </div>
-                                <div class="af-text" style="text-align: justify;">
+                                <div class="af-text">
                                     <h6>what we do</h6>
                                     <p>1. Sign up now and sell or rent your own properties</p>
                                     <p>2. Our experienced realtors strongly negotiate for you.</p>
@@ -91,7 +90,7 @@
     <section class="contact-section" style="margin-top: 5px;">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6">
+                <div class="col-lg-12">
                     <div class="contact-info">
                         <div class="ci-item">
                             <div class="ci-icon">
@@ -109,7 +108,7 @@
                             <div class="ci-text">
                                 <h5>Phone</h5>
                                 <ul>
-                                    <li>+234805544332211</li>
+                                    <li><i class="fa fa-mobile-phone" style="color: #a31317;"></i> 09023838128 | <span>08155994277</span></li>
                                 </ul>
                             </div>
                         </div>
@@ -119,18 +118,18 @@
                             </div>
                             <div class="ci-text">
                                 <h5>Support</h5>
-                                <p>vostan.support@vostan.com</p>
+                                <p>info@vostangroup.com/p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="cs-map">
+      <!--   <div class="cs-map">
             <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d735515.5813275519!2d-80.41163541934742!3d43.93644386501528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882a55bbf3de23d7%3A0x3ada5af229b47375!2sAwka%2C%20Anambra%2C%20Nigeria!5e0!3m2!1sen!2sbd!4v1583262687289!5m2!1sen!2sbd"
                 height="450" style="border:0;" allowfullscreen=""></iframe>
-        </div>
+        </div> -->
     </section>
     <!-- Contact Section End -->
 
